80412fadc430d564eac49039dfcd880bad66451c
Marissa Zhong <mwz2101@columbia.edu>
2018-07-09 20:28:03 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,8 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.integer :price
       t.string :image_url
       t.string :size
-      t.created_at
-      t.updated_at
+      t.timestamps
     end
   end
 end
