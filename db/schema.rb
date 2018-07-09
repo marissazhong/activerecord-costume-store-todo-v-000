a1179fd6d2fafcbe74d00d39e0297194782d5ea7
Marissa Zhong <mwz2101@columbia.edu>
2018-07-09 20:28:45 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "price"
     t.string "image_url"
     t.string "size"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
